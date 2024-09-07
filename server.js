85210af0051f4f6b5a859fc00a1bd5f66c3d28dc
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-07 14:49:57 +0100

    Created a route for menu.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/menu", (req, res) => {
+    
+});
+
 app.listen(5000, () => {
-    console.log("Server started at https://localhost:5000");
+    console.log("Server started at http://localhost:5000");
 });
