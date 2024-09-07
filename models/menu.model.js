cd84e44cd21f1b028c5d376f1b28048e205e3bd9
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-07 15:53:11 +0100

    Created a schema for menu.

---
+++ models/menu.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const menuSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    timestamps: true,
+});
