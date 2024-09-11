320bd2fe847dcc7bde5c00735ea798279c7b71ae
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-11 20:55:22 +0100

    Added logic to check if an auth0 user exists.

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
 const createCurrentUser = async (req: Request, res: Response) => {
     // 1. Check if the user exists
     // 2. If not create the user
     // 3. Return the user object to the calling client
+
+    try {
+        const { auth0Id } = req.body;
+        const existingUser = await User.findOne({ auth0Id });
+
+        if(existingUser){
+            return res.status(200).send();
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error creating user" })
+    }
 };
 
 export default {
