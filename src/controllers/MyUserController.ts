d73b731e26824ac0cb01bbb73fa0c269d902f35c
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-11 20:57:17 +0100

    Added logic to create auth0 user.

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -13,6 +13,10 @@ const createCurrentUser = async (req: Request, res: Response) => {
         if(existingUser){
             return res.status(200).send();
         }
+
+        const newUser = new User(req.body);
+        await newUser.save();
+
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Error creating user" })
