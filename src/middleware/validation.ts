875b5adb312cd6ff2bbe81d42c9b7812cd0287bc
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-12 20:40:53 +0100

    Added middleware to handle validation errors.

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,4 +1,17 @@
-import { body } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
+
+const handleValidationErrors = async (
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    };
+    next();
+};
 
 export const validateMyUserRequest = [
     body("name")
