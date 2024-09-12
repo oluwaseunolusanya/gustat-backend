554d7cb03ca04aa5248bab6d624b53ef8b727281
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-12 19:35:00 +0100

    Added auth0 token verifier function.

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
-const jwtCheck = auth({
-    audience: 'gustat-frontend-api',
-    issuerBaseURL: 'https://dev-uk80dmzvmpqhetln.uk.auth0.com/',
+export const jwtCheck = auth({
+    audience: process.env.AUTH0_AUDIENCE,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
     tokenSigningAlg: 'RS256'
 });
