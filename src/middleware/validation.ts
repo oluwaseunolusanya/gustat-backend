17ac923231523b4c870bc0694e918d726d73bcaa
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-12 20:36:44 +0100

    Installed express validator package and added validation logic for user profile fields.

---
+++ src/middleware/validation.ts
@@ -0,0 +1,20 @@
+import { body } from "express-validator";
+
+export const validateMyUserRequest = [
+    body("name")
+        .isString()
+        .notEmpty()
+        .withMessage("Name must be a string"),
+    body("addressLine1")
+        .isString()
+        .notEmpty()
+        .withMessage("Address Line1 must be a string"),
+    body("city")
+        .isString()
+        .notEmpty()
+        .withMessage("City must be a string"),
+    body("country")
+        .isString()
+        .notEmpty()
+        .withMessage("Country must be a string"),
+];
