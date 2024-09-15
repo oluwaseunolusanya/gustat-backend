b44bf9d0372094d4ba60e5bbb9afe66a377e73f1
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-15 21:22:10 +0100

    Added endpoint to check the health of server.

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req:Request, res: Response) => {
+    res.send({messge: "health OK!"});
+});
+
 app.use("/api/my/user", myUserRoute);
 
 app.listen(5000, () => {
