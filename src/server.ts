c44ec804baec2dda666fea77fb15af79209681e2
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-16 19:00:50 +0100

    Added cloudinary config.

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,18 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database!"));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+
+});
 
 // Create a new express server.
 const app = express();
