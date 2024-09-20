4939895bcea8afa65671495e5c63f8329c9d6319
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-20 20:29:18 +0100

    Added validation to my restaurant fields.

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -35,3 +35,37 @@ export const validateMyUserRequest = [
         .withMessage("Country must be a string"),
     handleValidationErrors, // Attach validation error handler
 ];
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName")
+        .notEmpty()
+        .withMessage("Restaurant name is required"),
+    body("city")
+        .notEmpty()
+        .withMessage("City is required"),
+    body("country")
+        .notEmpty()
+        .withMessage("Country is required"),
+    body("deliveryPrice")
+        .isFloat({ min:0 })
+        .withMessage("Delivery price must be a positive number"),
+    body("estimatedDeliveryTime")
+        .isInt({ min:0 })
+        .withMessage("Estimated delivery time be a positive integer"),
+    body("cuisines")
+        .isArray()
+        .withMessage("Cuisines must be an array")
+        .not()
+        .isEmpty()
+        .withMessage("Cuisines array cannot be empty"),
+    body("menuItems")
+        .isArray()
+        .withMessage("Menu items must be an array"),
+    body("menuItems.*.name")
+        .notEmpty()
+        .withMessage("Menu items name is required"),
+    body("menuItems.*.price")
+        .isFloat({ min: 0 })
+        .withMessage("Menu items price is required and must a positive number"),
+        handleValidationErrors, // Attach validation error handler
+]
